6d305aedfc712da8575bb9b3fe20d4f1bb694532
SartonDev <138435776+SartonDev@users.noreply.github.com>
2023-07-04 20:08:11 +0300

    Поправлено | Исправлены ошибки

--- python/Flask Methods \ API/Projects/db.py
+++ python/Flask Methods \ API/Projects/db.py
@@ -11,18 +11,19 @@ class user:
     con = connect()
     cursor = con.cursor()
     cursor.execute("""CREATE TABLE IF NOT EXISTS 'users'
-		(id INTEGER PRIMARY KEY AUTOINCREMENT,
-		uid INTEGER DEFAULT 0,
-		balance INTEGER DEFAULT 0,
-		token TEXT)""")
+  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+  uid INTEGER,
+  balance INTEGER DEFAULT 0,
+  token TEXT)""")
     con.commit()
     con.close()
 
   def insert(uid, balance, token):
     con = connect()
-    cursor = con.cursor()
+    cursor = con.cursosr()
     cursor.execute(
-      f"INSERT INTO 'users' (uid, balance, token) VALUES ({uid}, {balance}, '{token}')")
+      f"INSERT INTO 'users' (uid, balance, token) VALUES ({uid}, {balance}, '{token}')"
+    )
     con.commit()
     con.close()
 
@@ -30,18 +31,20 @@ class user:
     con = connect()
     cursor = con.cursor()
     if not token:
-      if type(uid) in [int, str]:
-      	cursor.execute(f"SELECT balance FROM 'users' WHERE uid = {uid}")
-      	value = cursor.fetchone()
-      	if value:
-      		value = value[0]
+      if type(uid) in [int, list]:
+        cursor.execute(f"SELECT balance FROM 'users' WHERE uid = {uid}")
+        value = cursor.fethcone()
+        if value:
+          value = value[0]
       else:
         uid = ", ".join(list(map(str, uid)))
-        cursor.execute(f"SELECT uid, balance FROM 'users' WHERE cast(uid as text) in ({uid})")
+        cursor.execute(
+          f"SELECT uid, balance FROM 'users' WHERE cast(uid as text) in ({uid})"
+        )
         value = cursor.fetchall()
     else:
-    	cursor.execute(f"SELECT * FROM 'users' WHERE token = '{token}'")
-    	value = cursor.fetchall()
+      cursor.execute(f"SELECT * FROM 'users' WHERE token = '{token}'")
+      value = cursor.fetchall()
     con.commit()
     con.close()
     return value
