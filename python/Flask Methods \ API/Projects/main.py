08afb8a3a2909a56312baaf0754abd0c13520f99
SartonDev <138435776+SartonDev@users.noreply.github.com>
2023-07-04 20:29:46 +0300

    Откат

--- python/Flask Methods \ API/Projects/main.py
+++ python/Flask Methods \ API/Projects/main.py
@@ -4,24 +4,13 @@ from threading import Thread
 from random import randint
 
 user.create()
+
 if not user.get(123):
-  for e in [{
-      'id': 123,
-      'token': 'hDie38NCXka'
-  }, {
-      'id': 145,
-      'token': 'uei32CCaks'
-  }, {
-      'id': 167,
-      'token': 'AEcDJKwk08'
-  }, {
-      'id': 189,
-      'token': '4778SHasqNC'
-  }, {
-      'id': 200,
-      'token': 'LQP10VX6SbnV'
-  }]:
-    user.insert(e['id'], randint(0, 100000), e['token'])
+  user.insert(123, randint(1, 10000), 'None')
+  user.insert(145, randint(1, 10000), 'None')
+  user.insert(167, randint(1, 10000), 'None')
+  user.insert(189, randint(1, 10000), 'None')
+  user.insert(200, randint(1, 10000), 'None')
 
 app = Flask('')
 
@@ -29,37 +18,6 @@ app = Flask('')
 @app.route('/')
 def index():
   return '<h1> Hello, World! </h1><br><h2> With love, SARTON DEV! </h2>'
-
-
-@app.route('/user')
-def get_user():
-  value = request.args.get('id')
-  if value:
-    user_data = user.get(value)
-    if user_data:
-      return str(user_data)
-    else:
-      return 'Unknown user.'
-  else:
-    return 'Unknown parameter "id".'
-
-
-@app.route('/user.get', methods=['GET', 'POST'])
-def post_user():
-  if request.method == "POST":
-    value = request.json
-    if type(value['id']) in [int, list]:
-      json = {'data': []}
-      data = user.get(value['id'])
-      if data:
-        for i in data:
-          json['data'].append({'user_id': i[0], 'balance': i[1]})
-          return jsonify(json)
-      else:
-        return jsonify({"Error": "Unknown user."})
-    else:
-      return jsonify({"Error": "Unsupported value."})
-
 
 def run():
   app.run(host='0.0.0.0', port=8080)
