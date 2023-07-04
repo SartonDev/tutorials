11b518d6e51a3a38c6ec1fa44bb147d1d625c254
SartonDev <138435776+SartonDev@users.noreply.github.com>
2023-07-04 20:07:51 +0300

    Изменено | Поправлены ошибки

--- python/Flask Methods \ API/Projects/main.py
+++ python/Flask Methods \ API/Projects/main.py
@@ -5,55 +5,69 @@ from random import randint
 
 user.create()
 if not user.get(123):
-  user.insert(189, randint(1,100000), "GheEufsbXJD")
-  user.insert(123, randint(1,100000), "DFSHJejkrwDS")
-  user.insert(167, randint(1,100000), "DHJFHuewyruGJgdhSG")
-  user.insert(200, randint(1,100000), "HjSHJieUINXh")
-  user.insert(145, randint(1,100000), "UIREUWIO832FDSdsk")
+  for e in [{
+      'id': 123,
+      'token': 'hDie38NCXka'
+  }, {
+      'id': 145,
+      'token': 'uei32CCaks'
+  }, {
+      'id': 167,
+      'token': 'AEcDJKwk08'
+  }, {
+      'id': 189,
+      'token': '4778SHasqNC'
+  }, {
+      'id': 200,
+      'token': 'LQP10VX6SbnV'
+  }]:
+    user.insert(e['id'], randint(0, 100000), e['token'])
 
 app = Flask('')
 
-@app.route("/")
+
+@app.route('/')
 def index():
-  return "Hello, World! It's me, SARTON!"
+  return '<h1> Hello, World! </h1><br><h2> With love, SARTON DEV! </h2>'
+
 
 @app.route('/user')
-def userget():
+def get_user():
   value = request.args.get('id')
   if value:
     user_data = user.get(value)
     if user_data:
-      return str(user.get(value))
+      return str(user_data)
     else:
-      return 'Unknown user'
+      return 'Unknown user.'
   else:
-    return 'Unknown parameter id'
+    return 'Unknown parameter "id".'
+
 
 @app.route('/user.get', methods=['GET', 'POST'])
-def userget():
+def post_user():
   if request.method == "POST":
     value = request.json
     if type(value['id']) in [int, list]:
-      json = {
-        'data': []
-      }
+      json = {'data': []}
       data = user.get(value['id'])
       if data:
         for i in data:
-          json['data'].append({
-            'user_id': i[0],
-            'balance': i[1]
-          })
+          json['data'].append({'user_id': i[0], 'balance': i[1]})
           return jsonify(json)
       else:
-        return jsonify({"Error": "Unknown user"})
+        return jsonify({"Error": "Unknown user."})
     else:
-      return jsonify({"Error": "Unsupported value"})
+      return jsonify({"Error": "Unsupported value."})
+
 
 def run():
-  app.run(host="0.0.0.0", port=8080)
+  app.run(host='0.0.0.0', port=8080)
+
 
 def th_start():
-  site = Thread(target=run).start()
+  site = Thread(target=run)
+  site.start()
+
 
 th_start()
